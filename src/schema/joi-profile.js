66f587413da38f2f23cae366a911431008fff434
ivan setiawan <ivansetiawanvll@gmail.com>
2023-05-27 20:58:17 +0700

    update schema profile

--- src/schema/joi-profile.js
+++ src/schema/joi-profile.js
@@ -1,9 +1,10 @@
 const Joi = require('joi');
 
-const profil=Joi.object({
-      phone:Joi.number().required(),
-      gender:Joi.string().required(),
-      address:Joi.string().required()
+const profil = Joi.object({
+  image: Joi.string(),
+  phone: Joi.number().required(),
+  gender: Joi.string().required(),
+  address: Joi.string().required(),
 });
 
 module.exports= profil;
